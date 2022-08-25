70fd7c5cc23865d57bdc29ddf5fb834a1b1c58f5
李東穎 <dean19990226@gmail.com>
2022-08-25 13:29:08 +0800

    nav

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,15 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+
+    $('#nav__buger').on('click',function(e){
+      $('#nav__buger').addClass('open');
+      $('#nav__close').addClass('open');
+      $('.nav__content__mobile__menu').addClass('open');
+    })
+
+    $('#nav__close').on('click',function(){
+      $('#nav__close').removeClass('open');
+      $('#nav__buger').removeClass('open');
+      $('.nav__content__mobile__menu').removeClass('open');
+    })
+  
 });
