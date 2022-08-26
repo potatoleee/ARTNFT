fd561c91e9f84aaec3792aeb18c641a0127c9915
李東穎 <dean19990226@gmail.com>
2022-08-26 16:17:16 +0800

    Update 2022-08-26T08:17:14.716Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,4 +12,24 @@ $(function () {
     $('.nav__content__mobile__menu').removeClass('open');
   });
 });
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 24,
+  centeredSlides: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+});
 //# sourceMappingURL=all.js.map
